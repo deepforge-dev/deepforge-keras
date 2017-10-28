463255a8ef8ddf371f863acd8d7d7d30b6b34304
Brian Broll <brian.broll@gmail.com>
2017-10-28 09:04:27 -0500

    WIP #4 added seed for keras nn meta

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -13,6 +13,7 @@ config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
 config.visualization.decoratorPaths.push(__dirname + '/../node_modules/webgme-easydag/src/decorators');
 config.seedProjects.basePaths.push(__dirname + '/../src/seeds/dev');
 config.seedProjects.basePaths.push(__dirname + '/../src/seeds/base');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/keras');
 
 
 
