3b4773f1d8dfda6c5096bca657962c8cd64f6286
Brian Broll <brian.broll@gmail.com>
2017-11-11 09:42:48 -0600

    WIP #28 removed 'click to view'

--- src/decorators/LayerDecorator/EasyDAG/LayerField.js
+++ src/decorators/LayerDecorator/EasyDAG/LayerField.js
@@ -1,29 +1,14 @@
-/* globals define, _ */
+/* globals define, _, WebGMEGlobal */
 define([
     'decorators/EllipseDecorator/EasyDAG/PointerField'
 ], function(
     PointerField
 ) {
-    // The LayerField behaves the same as PointerFields but it shows "click to view"
-    // if it has a value
     var LayerField = function() {
         PointerField.apply(this, arguments);
     };
 
     _.extend(LayerField.prototype, PointerField.prototype);
-
-    LayerField.prototype.getContent = function(content) {
-        return content && 'click to view';
-    };
-
-    LayerField.prototype.createContent = function(w, y, content) {
-        PointerField.prototype.createContent.call(this, w, y, this.getContent(content));
-        this.$content.attr('font-style', 'italic');
-    };
-
-    LayerField.prototype.setValue = function(content) {
-        PointerField.prototype.setValue.call(this, this.getContent(content));
-    };
 
     return LayerField;
 });
