ceaf24035c927a700015bb79a509bad9a9201076
Brian Broll <brian.broll@gmail.com>
2018-03-15 13:14:26 -0500

    WIP #49 added connect to/from buttons

--- src/visualizers/widgets/KerasArchEditor/Buttons.js
+++ src/visualizers/widgets/KerasArchEditor/Buttons.js
@@ -1,13 +1,104 @@
-/*globals define, WebGMEGlobal*/
+/*globals define, WebGMEGlobal, $, d3*/
 define([
     'widgets/EasyDAG/Buttons',
-    'widgets/EasyDAG/Icons'
+    'widgets/EasyDAG/Icons',
+    'underscore'
 ], function(
     Buttons,
-    Icons
+    Icons,
+    _
 ) {
 
     var client = WebGMEGlobal.Client;
+    var ConnectToInput = function(params) {
+        Buttons.ButtonBase.call(this, params);
+    };
+    ConnectToInput.SIZE = 10;
+    ConnectToInput.BORDER = 1;
+    ConnectToInput.prototype.BTN_CLASS = 'connect-to-input';
+    ConnectToInput.prototype = new Buttons.ButtonBase();
+
+    ConnectToInput.prototype._render = function() {
+        var lineRadius = ConnectToInput.SIZE - ConnectToInput.BORDER,
+            btnColor = '#90caf9';
+
+        if (this.disabled) {
+            btnColor = '#e0e0e0';
+        }
+
+        this.$el
+            .append('circle')
+            .attr('r', ConnectToInput.SIZE)
+            .attr('fill', btnColor);
+
+        // Show the 'code' icon
+        Icons.addIcon('chevron-bottom', this.$el, {
+            radius: lineRadius
+        });
+    };
+
+    ConnectToInput.prototype._onClick = function(inputId) {
+        if (d3.event.shiftKey) {
+            this.onAddButtonClicked(inputId);
+        } else {
+            this.startConnectionFrom(inputId);
+
+            if (this.selectionManager.selectedItem) {
+                const selectedId = this.selectionManager.selectedItem.id;
+
+                if (selectedId.includes(inputId)) {
+                    this.selectionManager.deselect();
+                }
+            }
+        }
+    };
+    Buttons.ConnectToInput = ConnectToInput;
+
+    var ConnectToOutput = function(params) {
+        Buttons.ButtonBase.call(this, params);
+    };
+    ConnectToOutput.SIZE = 10;
+    ConnectToOutput.BORDER = 1;
+    ConnectToOutput.prototype.BTN_CLASS = 'connect-to-input';
+    ConnectToOutput.prototype = new Buttons.ButtonBase();
+
+    ConnectToOutput.prototype._render = function() {
+        var lineRadius = ConnectToOutput.SIZE - ConnectToOutput.BORDER,
+            btnColor = '#90caf9';
+
+        if (this.disabled) {
+            btnColor = '#e0e0e0';
+        }
+
+        this.$el
+            .append('circle')
+            .attr('r', ConnectToOutput.SIZE)
+            .attr('fill', btnColor);
+
+        // Show the 'code' icon
+        Icons.addIcon('chevron-bottom', this.$el, {
+            radius: lineRadius
+        });
+    };
+
+    ConnectToOutput.prototype._onClick = function(inputId) {
+        if (d3.event.shiftKey) {
+            this.onAddButtonClicked(inputId);
+        } else {
+            this.startConnectionFrom(inputId);
+
+            if (this.selectionManager.selectedItem) {
+                const selectedId = this.selectionManager.selectedItem.id;
+
+                if (selectedId.includes(inputId)) {
+                    this.selectionManager.deselect();
+                }
+            }
+        }
+    };
+    Buttons.ConnectToOutput = ConnectToOutput;
+
+
     var GoToBase = function(params) {
         // Check if it should be disabled
         var baseId = this._getBaseId(params.item),
