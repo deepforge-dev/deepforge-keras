728234fffd56b5fd8abfda47aa20363b69169289
Brian Broll <brian.broll@vanderbilt.edu>
2018-04-12 09:44:57 -0500

    WIP #71 show the connection index, if set

--- src/visualizers/widgets/KerasArchEditor/Connection.js
+++ src/visualizers/widgets/KerasArchEditor/Connection.js
@@ -6,13 +6,36 @@ define([
     BaseConnection,
     _
 ) {
-    var Connection = function() {
+    var Connection = function($parent, desc) {
         BaseConnection.apply(this, arguments);
+        this.$index = this.$el.append('g')
+            .attr('class', 'connection-index');
+
+        this.update(desc);
     };
 
     _.extend(Connection.prototype, BaseConnection.prototype);
 
     Connection.prototype.onClick = function() {
+    };
+
+    Connection.prototype.redraw = function() {
+        BaseConnection.prototype.redraw.apply(this, arguments);
+        // More intelligent placement of indices
+        // TODO
+        const center = {
+            x: this.x,// + this.getWidth()/2,
+            y: this.y// - this.getHeight()/2
+        };
+        this.$index.attr('transform', `translate(${center.x+10}, ${center.y})`);
+    };
+
+    Connection.prototype.update = function(desc) {
+        BaseConnection.prototype.update.apply(this, arguments);
+        if (desc.index !== null) {
+            this.$index.append('text')
+                .text(desc.index);
+        }
     };
 
     return Connection;
