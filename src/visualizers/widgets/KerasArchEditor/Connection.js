adf4a48f60ea97b8c1e4321b7bca95518cd19b0b
Brian Broll <brian.broll@gmail.com>
2018-05-17 13:34:24 -0500

    WIP Added extra padding to connections

--- src/visualizers/widgets/KerasArchEditor/Connection.js
+++ src/visualizers/widgets/KerasArchEditor/Connection.js
@@ -1,4 +1,4 @@
-/*globals define*/
+/*globals define, d3*/
 define([
     'widgets/EasyDAG/Connection',
     'underscore'
@@ -6,24 +6,50 @@ define([
     BaseConnection,
     _
 ) {
+    const lineFn = d3.svg.line()
+        .x(d => d.x)
+        .y(d => d.y)
+        .interpolate('linear');
+
     var Connection = function($parent, desc) {
         BaseConnection.apply(this, arguments);
         this.$index = this.$el.append('g')
             .attr('class', 'connection-index');
 
         this.$indexText = this.$index.append('text');
+        this.$padding = this.$el.append('path')
+            .attr('stroke-width', 12)
+            .attr('class', 'conn-padding')
+            .attr('stroke-opacity', 0.01)
+            .attr('fill', 'none');
+
+        this.$padding
+            .on('mouseover', () => this.onMouseOver())
+            .on('mouseout', () =>  this.onMouseOut());
+
         this.update(desc);
     };
 
     _.extend(Connection.prototype, BaseConnection.prototype);
 
-    Connection.prototype.onClick = function() {
+    Connection.prototype.onClick = function() {};
+
+    Connection.prototype.onMouseOver = function() {
+        this.$path.attr('stroke-width', 3);
+    };
+
+    Connection.prototype.onMouseOut = function() {
+        this.$path.attr('stroke-width', 2);
     };
 
     Connection.prototype.redraw = function() {
         BaseConnection.prototype.redraw.apply(this, arguments);
-        // More intelligent placement of indices
-        // TODO
+
+        this.$padding.attr('d', lineFn(this.points))
+            .transition()
+            .attr('stroke', '#000000ee')
+            .attr('fill', 'none');
+
         this.$index.attr('transform', `translate(${this.x+10}, ${this.y})`);
     };
 
