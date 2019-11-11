b03decb15a71870a351b39d1534614b68bd1ff97
Brian Broll <brian.broll@gmail.com>
2019-11-11 08:01:05 -0600

    Infer type based on name if undefined. Fixes #118 (#132)
    * Infer type based on name if undefined. Fixes #118
    
    * Add tests for schemas
    
    * WIP Update schema save location in parse script
    
    * WIP #118 Update CreateKerasMeta to use JSONImporter
    
    * WIP update keras seed
    
    * WIP increase timeout

---
+++ src/plugins/CreateKerasMeta/schemas/index.js
@@ -0,0 +1,47 @@
+/*globals define*/
+define([
+    'text!./additional-layers.json',
+    'text!./activations.json',
+    'text!./initializers.json',
+    'text!./constraints.json',
+    'text!./regularizers.json',
+    'text!./layers.json',
+], function(
+    AdditionalLayerTxt,
+    ActivationsTxt,
+    InitializersTxt,
+    ConstraintsTxt,
+    RegularizersTxt,
+    LayerTxt,
+) {
+
+    const SpecialTypes = {
+        activation: JSON.parse(ActivationsTxt),
+        constraint: JSON.parse(ConstraintsTxt),
+        regularizer: JSON.parse(RegularizersTxt),
+        initializer: JSON.parse(InitializersTxt)
+    };
+    const SpecialTypeNames = Object.keys(SpecialTypes);
+    const AdditionalLayers = JSON.parse(AdditionalLayerTxt);
+    const Layers = JSON.parse(LayerTxt).concat(AdditionalLayers)
+        .map(layer => {  // apply any special case patching
+            if (layer.name === 'Wrapper') {
+                layer.arguments[1].type = 'Layer';
+            }
+            if (layer.name === 'Bidirectional') {
+                layer.arguments[1].type = 'Recurrent';
+            }
+
+            if (layer.arguments) {
+                layer.arguments.forEach(argument => {
+                    if (!argument.type) {
+                        argument.type = SpecialTypeNames
+                            .find(name => argument.name.includes(name));
+                    }
+                });
+            }
+            return layer;
+        });
+
+    return {SpecialTypes, Layers};
+});
