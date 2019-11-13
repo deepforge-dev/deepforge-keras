0c00613ee3d08448bddbcbfff6bef7e212f2e016
Umesh <umesh.timalsina@vanderbilt.edu>
2019-11-13 15:38:17 -0600

    #52: Refactor ExportKeras.spec.js, remove filename input from plugin metadata, change python version from 3.5 to 3

--- src/plugins/ExportKeras/ExportKeras.js
+++ src/plugins/ExportKeras/ExportKeras.js
@@ -3,12 +3,10 @@
 define([
     'plugin/PluginConfig',
     'text!./metadata.json',
-    'SimpleNodes/Constants',
     'plugin/GenerateKeras/GenerateKeras/GenerateKeras'
 ], function (
     PluginConfig,
     pluginMetadata,
-    SimpleConstants,
     PluginBase) {
     'use strict';
 
@@ -17,7 +15,6 @@ define([
     function ExportKeras() {
         PluginBase.call(this);
         this.pluginMetadata = pluginMetadata;
-        this.outputFileName = '';
     }
 
     ExportKeras.metadata = pluginMetadata;
@@ -28,14 +25,6 @@ define([
 
     ExportKeras.prototype.createOutputFiles = async function(activeNode){
         const files = PluginBase.prototype.createOutputFiles.call(this, activeNode);
-        if(!this.outputFileName){
-            let fileName = this.getCurrentConfig().jsonFileName || 'exported_architecture.json';
-            if(!fileName.endsWith('.json')){
-                fileName = `${fileName}.json`;
-            }
-            this.outputFileName = fileName;
-        }
-
         let outputFiles = {};
         let codeString = files['output.py'];
         const modelJSONName = this.generateVariableName('model_json');
@@ -44,24 +33,27 @@ define([
         code.push('');
         code.push(`${modelJSONName} = ${this.RESULT_VARIABLE_NAME}.to_json()`);
         code.push(`print(${modelJSONName})`);
-        outputFiles[this.outputFileName] = await this.executePython(code.join('\n'));
-        this.logger.info(`Exported JSON Model filename is ${this.outputFileName}`);
+        outputFiles[`${activeNode.name}.json`] = await this.executePython(code.join('\n'));
+        this.logger.info(`Exported JSON Model filename is ${activeNode.name}.json`);
         return outputFiles;
     };
 
-    ExportKeras.prototype.executePython = function (code, pythonVersion='3.5') {
+    ExportKeras.prototype.executePython = function (code, pythonVersion='3') {
+        if(!pythonVersion.startsWith('3')){
+            throw new Error(`Python${pythonVersion} is not supported`);
+        }
         const command = `python${pythonVersion} - <<EOF\n${code}\nEOF`;
         return new Promise((resolve, reject) => {
             exec(command, {
                 shell: '/bin/bash'
             },(error, stdout) => {
                 if(error) reject(error.toString());
-                if(stdout) resolve(this._stdoutToJSON(stdout));
+                if(stdout) resolve(this.getModelJSON(stdout));
             });
         });
     };
 
-    ExportKeras.prototype._stdoutToJSON = function (stdout) {
+    ExportKeras.prototype.getModelJSON = function (stdout) {
         let jsonModelString = stdout.split('\n').filter(val => !!val);
         jsonModelString = jsonModelString[jsonModelString.length-1];
         const jsonExport = JSON.stringify(JSON.parse(jsonModelString), null, 2);
--- test/plugins/ExportKeras/ExportKeras.spec.js
+++ test/plugins/ExportKeras/ExportKeras.spec.js
@@ -71,84 +71,120 @@ describe('ExportKeras', function () {
 
     describe('multi arch output', function () {
         let modelJSON;
+        let layers;
         const layerNames = ['input_1', 'activation_1', 'dense_1', 'dropout_1'];
         const layerClassNames = ['InputLayer', 'Activation', 'Dense', 'Dropout'];
 
         before(async function () {
             this.timeout(5000);
             modelJSON = await getGeneratedJSON(ARCHITECTURE.MultiArchOutputs);
+            layers = modelJSON.config.layers;
         });
 
-        it('should match layer names and number of layers', function () {
-            assert.equal(modelJSON.class_name, 'Model');
-            assert.equal(modelJSON.config.output_layers.length, 3,
-                `Number of Output layers should be 3 found ${modelJSON.config.output_layers.length}`);
+        it('should have correct number of layers', function () {
             assert.equal(modelJSON.config.layers.length, 4,
                 `Number of layers should be 3 found ${modelJSON.config.layers.length}`);
+        });
+
+        it('should have correct number of output layers', function () {
+            assert.equal(modelJSON.config.output_layers.length, 3,
+                `Number of Output layers should be 3 found ${modelJSON.config.output_layers.length}`);
+        });
+
+        it('should have correct layer names', function () {
             const layers = modelJSON.config.layers;
+            layers.forEach((layer, index) => {
+                assert.equal(layer.name, layerNames[index],
+                    `Mismatch in names for the layers expected
+                    ${layerNames[index]}, found ${layer.name}`);
+            });
+        });
+
+        it('should have correct layer types', function () {
             layers.forEach((layer, index) => {
                 assert.equal(layer.class_name, layerClassNames[index],
                     `Mismatch in class name for the layer expected
                     ${layerClassNames[index]}, found ${layer.class_name}`);
-                assert.equal(layer.name, layerNames[index],
-                    `Mismatch in names for the layers expected
-                    ${layerNames[index]}, found ${layer.name}`);
             });
         });
     });
 
     describe('nested (wrapped) layers', function () {
         let modelJSON;
+        let layers;
         const layerNames = ['input_1', 'time_distributed_1'];
         const layerClassNames = ['InputLayer', 'TimeDistributed'];
 
         before(async function () {
             this.timeout(5000);
             modelJSON = await getGeneratedJSON(ARCHITECTURE.NestedLayers);
+            layers = modelJSON.config.layers;
         });
 
-        it('should match layer names and number of layers', function () {
-            assert.equal(modelJSON.class_name, 'Model');
-            assert.equal(modelJSON.config.output_layers.length, 1,
-                `Number of Output layers should be 1 found ${modelJSON.config.output_layers.length}`);
+        it('should have correct number of layers', function () {
             assert.equal(modelJSON.config.layers.length, 2,
                 `Number of layers should be 2 found ${modelJSON.config.layers.length}`);
+        });
+
+        it('should have correct number of output layers', function () {
+            assert.equal(modelJSON.config.output_layers.length, 1,
+                `Number of Output layers should be 1 found ${modelJSON.config.output_layers.length}`);
+        });
+
+        it('should have correct layer names', function () {
             const layers = modelJSON.config.layers;
+            layers.forEach((layer, index) => {
+                assert.equal(layer.name, layerNames[index],
+                    `Mismatch in names for the layers expected
+                    ${layerNames[index]}, found ${layer.name}`);
+            });
+        });
+
+        it('should have correct layer types', function () {
             layers.forEach((layer, index) => {
                 assert.equal(layer.class_name, layerClassNames[index],
                     `Mismatch in class name for the layer expected
                     ${layerClassNames[index]}, found ${layer.class_name}`);
-                assert.equal(layer.name, layerNames[index],
-                    `Mismatch in names for the layers expected
-                    ${layerNames[index]}, found ${layer.name}`);
             });
         });
     });
 
     describe('seq to seq', function () {
         let modelJSON;
+        let layers;
         const layerNames = ['input_2', 'input_1', 'lstm_1', 'lstm_2', 'dense_1'];
         const layerClassNames = ['InputLayer', 'InputLayer', 'LSTM', 'LSTM', 'Dense'];
 
         before(async function () {
             this.timeout(5000);
             modelJSON = await getGeneratedJSON(ARCHITECTURE.Seq2Seq);
+            layers = modelJSON.config.layers;
         });
 
-        it('should match layer names and number of layers', function () {
-            assert.equal(modelJSON.class_name, 'Model');
-            assert.equal(modelJSON.config.output_layers.length, 1,
-                `Number of Output layers should be 1 found ${modelJSON.config.output_layers.length}`);
+        it('should have correct number of layers', function () {
             assert.equal(modelJSON.config.layers.length, 5,
                 `Number of layers should be 5 found ${modelJSON.config.layers.length}`);
+        });
+
+        it('should have correct number of output layers', function () {
+            assert.equal(modelJSON.config.output_layers.length, 1,
+                `Number of Output layers should be 1 found ${modelJSON.config.output_layers.length}`);
+        });
+
+        it('should have correct layer names', function () {
             const layers = modelJSON.config.layers;
+            layers.forEach((layer, index) => {
+                assert.equal(layer.name, layerNames[index],
+                    `Mismatch in names for the layers expected
+                    ${layerNames[index]}, found ${layer.name}`);
+            });
+        });
+
+        it('should have correct layer types', function () {
             layers.forEach((layer, index) => {
                 assert.equal(layer.class_name, layerClassNames[index],
                     `Mismatch in class name for the layer expected
                     ${layerClassNames[index]}, found ${layer.class_name}`);
-                assert.equal(layer.name, layerNames[index],
-                    `Mismatch in names for the layers expected
-                    ${layerNames[index]}, found ${layer.name}`);
             });
         });
     });
