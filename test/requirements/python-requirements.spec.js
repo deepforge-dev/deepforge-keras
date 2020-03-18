12cd0cc29a16653c07abbfc9dbaa653dcf7c87fa
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-03-18 17:49:12 -0500

    Install conda in travis. Close #168

--- test/requirements/python-requirements.spec.js
+++ test/requirements/python-requirements.spec.js
@@ -7,15 +7,14 @@ const fs = require('fs');
 const path = require('path');
 
 describe('python', function () {
-    it('should find python 3.5', () => {
+    it('should find python 3.7', () => {
         const processOutput = spawnSync('python3', ['-c', `import sys; print(sys.version[0:3])`]);
         const pythonVersion = processOutput.stdout.toString().trim();
-        assert.equal(pythonVersion, '3.5', `Expected python version 3.5 but found ${pythonVersion}`);
+        assert.equal(pythonVersion, '3.7', `Expected python version 3.7 but found ${pythonVersion}`);
     });
 
     describe('requirements', function() {
-        const requirements = fs.readFileSync(path.join(__dirname, '..', '..', 'requirements.txt'),'utf-8').split('\n')
-            .map(line => line.trim()).filter(line => !!line);
+        const requirements = ['tensorflow', 'keras']
         requirements.forEach((req) => {
             it(`should be able to import package ${req}`, () =>{
                 const processOutput = spawnSync(`python3`, ['-c', `"import ${req}";`]);
